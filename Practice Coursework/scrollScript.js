2aa1ee4ee3522449e918c2953df334865b249ad4
barte <bartekgodon@yahoo.co.uk>
2018-03-01 14:29:08 +0000

    added basic search form

--- Practice Coursework/scrollScript.js
+++ Practice Coursework/scrollScript.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
   $(window).scroll(function () {
     if ($(window).scrollTop() > $('#headerBanner').height()) {
       $('#navigationBar').addClass('navbar-fixed');
+        $('#pageContent').addClass('padding');
     }
     if ($(window).scrollTop() < $('#headerBanner').height()) {
       $('#navigationBar').removeClass('navbar-fixed');
+        $('#pageContent').addClass('padding');
     }
   });
 });
