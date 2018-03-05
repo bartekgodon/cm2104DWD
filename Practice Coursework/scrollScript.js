748c21b517c07ea73ea1e8591c2f297ee7cff493
barte <bartekgodon@yahoo.co.uk>
2018-03-05 21:19:48 +0000

    updated the search area (split into tabs)

--- Practice Coursework/scrollScript.js
+++ Practice Coursework/scrollScript.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   $(window).scroll(function () {
     if ($(window).scrollTop() > $('#headerBanner').height()) {
       $('#navigationBar').addClass('navbar-fixed');
-        $('#pageContent').addClass('padding');
+        $('#searchHead').css("padding-top","92px");
     }
     if ($(window).scrollTop() < $('#headerBanner').height()) {
       $('#navigationBar').removeClass('navbar-fixed');
-        $('#pageContent').addClass('padding');
+        $('#searchHead').css("padding-top","30px");
     }
   });
 });
