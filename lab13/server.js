211a39f20f18547393c5adc2aef3221426ea335f
BARTLOMIEJ GODON (1604798) <1604798@rgu.ac.uk>
2018-03-12 15:50:02 +0000

    saas

--- lab13/server.js
+++ lab13/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var oneLinerJoke = require("one-liner-joke");
 
 app.get('/', function(req, res){
  res.send("Hello world! by express");
@@ -7,6 +8,12 @@ app.get('/', function(req, res){
 
 app.get('/test', function(req, res){
  res.send("this is route 2");
+});
+
+app.get('/joke', function(req, res){
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.send(oneLinerJoke.getRandomJoke());
+ res.end(randomJoke.body);
 });
 
 app.listen(8080);
