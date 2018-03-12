d61b21ba8025d7dceff7edae798c84a8a7852b38
BARTLOMIEJ GODON (1604798) <1604798@rgu.ac.uk>
2018-03-12 15:28:32 +0000

    l13

--- lab13/mymodule.js
+++ lab13/mymodule.js
@@ -1,8 +1,3 @@
-var http = require("http");
-var currentDate = require("./mymodule");
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('The date and time currently: ' + currentdate.myDateTime());
-  res.end('Hello World!');
-}).listen(8080);
+exports.myDateTime = function() {
+ return Date();
+};
