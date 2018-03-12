863d1cba056c94a952e5c4dbb3dce61711e6b0a5
BARTLOMIEJ GODON (1604798) <1604798@rgu.ac.uk>
2018-03-12 16:25:02 +0000

    sadsad

--- lab13/server.js
+++ lab13/server.js
@@ -38,5 +38,11 @@ app.get('/calc', function(req, res){
 
 });
 
+app.get('/getform', function(req, res){
+  var name = req.query.name;
+  var quest = req.query.quest;
+  res.send("Hi "+name+" I am sure you will "+quest) ;
+});
+
 app.listen(8080);
 app.use(express.static('public'));
