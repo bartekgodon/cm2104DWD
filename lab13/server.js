9da5cccb3231d3b8728fe56335d84657a92beb61
BARTLOMIEJ GODON (1604798) <1604798@rgu.ac.uk>
2018-03-12 15:53:34 +0000

    sadasd

--- lab13/server.js
+++ lab13/server.js
@@ -14,4 +14,10 @@ app.get('/joke', function(req, res){
  res.send(oneLinerJoke.getRandomJoke());
 });
 
+app.get('/add', function(req, res){
+  var x = req.query.x;
+  var y = req.query.y;
+ res.send("X + Y="+(x+y));
+});
+
 app.listen(8080);
