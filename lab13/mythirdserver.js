73dfff9853fade23d5b5bac8c8735108424609c9
BARTLOMIEJ GODON (1604798) <1604798@rgu.ac.uk>
2018-03-12 15:38:01 +0000

    sdasad

--- lab13/mythirdserver.js
+++ lab13/mythirdserver.js
@@ -1,5 +1,5 @@
 var http = require("http");
-var currentdate = require("one-liner-joke");
+var oneLinerJoke = require("one-liner-joke");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
