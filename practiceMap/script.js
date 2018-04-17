3ace48a8987734beb1bb5be3ac3521e9c4652832
barte <bartekgodon@yahoo.co.uk>
2018-04-17 15:39:07 +0100

    category seacrh works

--- practiceMap/script.js
+++ practiceMap/script.js
@@ -1,16 +1,3 @@
-// var map;
-//
-// function initMap() {
-//   geocoder = new google.maps.Geocoder();
-//   var aberdeen = {lat: 57.1497, lng: -2.0943};
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 13,
-//     center: aberdeen,
-//     disableDefaultUI: true
-//     }
-//   );
-// };
-
 var geocoder;
 var map;
 var service;
@@ -26,18 +13,34 @@ function initialize() {
       zoom: 15,
       disableDefaultUI: true
     });
-//new method here
+};
+
+function categorySearch() {
+
+  geocoder = new google.maps.Geocoder();
+  aberdeen = new google.maps.LatLng(57.1497,-2.0942);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: aberdeen,
+      zoom: 15,
+      disableDefaultUI: true
+    });
+
+  if(document.getElementById("categorySelect").value == "0"){return};
+
   var request = {
     location: aberdeen,
     radius: '2000',
-    type: ['bar']
+    type: [document.getElementById("categorySelect").value]
   };
 
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
 }
 //end
+
 function callback(results, status) {
+
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
@@ -47,17 +50,29 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
+  var bounds = new google.maps.LatLngBounds();
+  var placesList = document.getElementById('places');
   var placeLoc = place.geometry.location;
+
+  var image = {
+           url: place.icon,
+           size: new google.maps.Size(71, 71),
+           origin: new google.maps.Point(0, 0),
+           anchor: new google.maps.Point(17, 34),
+           scaledSize: new google.maps.Size(25, 25)
+         };
+
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
+  var li = document.createElement('li');
+         li.textContent = place.name;
+         placesList.appendChild(li);
+
+         bounds.extend(place.geometry.location);
+};
 
 function codeAddress() {
 
@@ -78,3 +93,11 @@ function codeAddress() {
     }
   });
 }
+
+  function clearMarkers() {
+    setMapOnAll(null);
+  };
+
+  function deleteMarkers() {
+        clearMarkers();
+  };
