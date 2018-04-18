0206a98657920bfb52ac9946f1d42e33c5b9d738
barte <bartekgodon@yahoo.co.uk>
2018-04-18 17:52:38 +0100

    category search works

--- practiceMap/script.js
+++ practiceMap/script.js
@@ -1,10 +1,13 @@
+//define required variables
 var geocoder;
 var map;
 var service;
 var infowindow;
 var aberdeen;
 
+  //initialize map on aberdeen
 function initialize() {
+
   geocoder = new google.maps.Geocoder();
   aberdeen = new google.maps.LatLng(57.1497,-2.0942);
 
@@ -15,63 +18,96 @@ function initialize() {
     });
 };
 
+//search for places based on category
 function categorySearch() {
+  //check if all required search parameters are valid, if not return
+  if(document.getElementById("categorySelect").value == "0" || document.getElementById("categorySelect").value < 1){return};
 
-  geocoder = new google.maps.Geocoder();
-  aberdeen = new google.maps.LatLng(57.1497,-2.0942);
+  //reset map to remove markers
+  initialize();
 
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: aberdeen,
-      zoom: 15,
-      disableDefaultUI: true
-    });
+  //get the places div (result output)
+  var div = document.getElementById('places');
 
-  if(document.getElementById("categorySelect").value == "0"){return};
+  //remove any outputs in the results div
+  while(div.firstChild){
+    div.removeChild(div.firstChild);
+  };
 
+  //generate the search request
   var request = {
     location: aberdeen,
-    radius: '2000',
+    radius: [document.getElementById("radius").value * 1000] ,
     type: [document.getElementById("categorySelect").value]
   };
 
+  //run the nearby search and generate an array of results
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
 }
-//end
 
+
+//callback method to create a marker for each result
 function callback(results, status) {
 
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
+      //create a marker for the current index place object
       createMarker(results[i]);
     }
   }
 }
 
+//create a marker, place it on a map and generate a place result to be displayed in the results div
 function createMarker(place) {
-  var bounds = new google.maps.LatLngBounds();
-  var placesList = document.getElementById('places');
-  var placeLoc = place.geometry.location;
 
-  var image = {
-           url: place.icon,
-           size: new google.maps.Size(71, 71),
-           origin: new google.maps.Point(0, 0),
-           anchor: new google.maps.Point(17, 34),
-           scaledSize: new google.maps.Size(25, 25)
-         };
+    //select the places div
+    var placesList = document.getElementById('places');
+    //get the geometry location of the input place
+    var placeLoc = place.geometry.location;
 
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+    //generate an icon image for the place
+    var image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
 
-  var li = document.createElement('li');
-         li.textContent = place.name;
-         placesList.appendChild(li);
+    //crete a new marker
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      position: place.geometry.location
+    });
 
-         bounds.extend(place.geometry.location);
+
+    //generate place result info div
+    var div = document.createElement('div');
+    div.setAttribute("id","resultDiv");
+
+    var h3 = document.createElement("h3");
+    h3.setAttribute("id","resultHead");
+
+    var p = document.createElement("p");
+    p.setAttribute("id","resultPara");
+
+    var button = document.createElement("BUTTON");
+    var t = document.createTextNode("ADD");
+    button.appendChild(t);
+
+    h3.textContent = place.name;
+    p.textContent = place.vicinity;
+
+    div.appendChild(h3);
+    div.appendChild(p);
+    div.appendChild(button);
+
+    //add info div into the result list
+    placesList.appendChild(div);
+
 };
 
 function codeAddress() {
@@ -93,11 +129,3 @@ function codeAddress() {
     }
   });
 }
-
-  function clearMarkers() {
-    setMapOnAll(null);
-  };
-
-  function deleteMarkers() {
-        clearMarkers();
-  };
